67259c0e7213167ede41bd3be4fa93fa76d30edb
edwardk96 <edwardkerr96@btinternet.com>
2025-01-07 21:49:34 +0000

    Simplified design

--- script.js
+++
@@ -1,7 +0,0 @@
-// Hamburger Menu Toggle
-const hamburgerBtn = document.getElementById('hamburger-btn');
-const navMenu = document.getElementById('nav-menu');
-
-hamburgerBtn.addEventListener('click', () => {
-  navMenu.classList.toggle('show');
-});
